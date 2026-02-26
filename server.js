ef044f9eaae3c4c167254a3cf60f5911fbbdace1
vishuu kumar <vishusingh91521@gmail.com>
2026-02-26 17:27:58 +0530

    fix

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5500",
+    "http://127.0.0.1:5500",
     "http://localhost:3000",
     "https://ai-calling-frontend.onrender.com"
   ],
