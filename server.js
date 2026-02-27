ab3cc88cc654c2a514907530ebeee2c4997bf21c
vishuu kumar <vishusingh91521@gmail.com>
2026-02-27 10:08:20 +0530

    fix

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:5501",
+    "http://127.0.0.1:5501",
     "http://localhost:3000",
     "https://ai-calling-frontend.onrender.com"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
