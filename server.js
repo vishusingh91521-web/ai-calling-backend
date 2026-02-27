63ef10b34458ee0dd0461139f9aa1fa21580b743
vishuu kumar <vishusingh91521@gmail.com>
2026-02-27 12:07:31 +0530

    fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 
 // ================== MIDDLEWARES ==================
 app.use(express.json());
